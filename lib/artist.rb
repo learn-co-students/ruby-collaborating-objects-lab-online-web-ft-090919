966cd025767d2b8dd0254406420ba308ce074646
Kenneth Spatta <kenyounot@gmail.com>
2019-09-18 05:28:14 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,18 +28,19 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    song.artist = self
   end
 
   def print_songs
-    songs.each { |song| puts song.name }
+    songs.each {|song| puts song.name}
   end
 
   def save
     @@all << self
   end
   
-  def songs 
-    @songs
+ def songs
+   @songs
+    Song.all.select {|song| song.artist == self}
   end
 end
